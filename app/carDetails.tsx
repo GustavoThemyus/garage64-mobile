9b273b28d0661837d929fbb941c2151bc658e364
Gustavo Themyus <gustavothemyus@gmail.com>
2026-08-04 09:58:14 -0300

    feat: add localSearchParams for carDetails

--- app/carDetails.tsx
+++ app/carDetails.tsx
@@ -1,17 +1,11 @@
 // Imports
-import { carsMoch } from "@/src/data/carsMoch"; // Import dos dados mocados dos carros
-import {
-  FlatList,
-  Text,
-  View,
-  useWindowDimensions,
-  StyleSheet,
-} from "react-native"; // Imports dos componentes do React Native
-import { useState } from "react"; // Import do useState do React
-import { Link } from "expo-router"; // Import do Link do Expo Router. Ele é usado pra navegação entre telas. É diferente do Stack porque não tem uma pilha de telas, e sim uma navegação entre elas.
+import { Text, View, StyleSheet } from "react-native"; // Imports dos componentes do React Native
+import { useLocalSearchParams } from "expo-router";
 
 // Função da tela de detalhes do carro.
 export default function CarDetails() {
+  const { carId } = useLocalSearchParams();
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Details</Text>{" "}
