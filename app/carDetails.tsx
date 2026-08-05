5d511b106c81fd80876cf8865601d41ada4352e1
Gustavo Themyus <gustavothemyus@gmail.com>
2026-08-05 01:26:59 -0300

    add const selectedCar for the car that's selected

--- app/carDetails.tsx
+++ app/carDetails.tsx
@@ -1,14 +1,16 @@
 // Imports
+import { carsMoch } from "@/src/data/carsMoch";
 import { Text, View, StyleSheet } from "react-native"; // Imports dos componentes do React Native
 import { useLocalSearchParams } from "expo-router";
 
 // Função da tela de detalhes do carro.
 export default function CarDetails() {
   const { carId } = useLocalSearchParams();
+  const selectedCar = carsMoch.find((car) => car.info.id === Number(carId));
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Details</Text>{" "}
+      <Text style={styles.title}>Details</Text>
       {/* Por enquanto apenas um título simples */}
     </View>
   );
