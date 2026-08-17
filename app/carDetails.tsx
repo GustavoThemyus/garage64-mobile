9ae10e8d747ad92fe6225b5ff1eedcda61c39a14
Gustavo Themyus <gustavothemyus@gmail.com>
2026-08-17 10:42:04 -0300

    feat: add imported style from useTheme on the title

--- app/carDetails.tsx
+++ app/carDetails.tsx
@@ -2,17 +2,19 @@
 import { carsMoch } from "@/src/data/carsMoch";
 import { Text, View, StyleSheet } from "react-native"; // Imports dos componentes do React Native
 import { useLocalSearchParams } from "expo-router";
+import { useTheme } from "@/src/theme/useTheme";
 
 // Função da tela de detalhes do carro.
 export default function CarDetails() {
+  const theme = useTheme();
   const { carId } = useLocalSearchParams();
   const selectedCar = carsMoch.find((car) => car.info.id === Number(carId));
   if (!selectedCar) {
     return <Text>Not found</Text>;
   }
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{selectedCar.info.model}</Text>
+    <View style={[styles.container, { backgroundColor: theme.background }]}>
+      <Text style={{ color: theme.textPrimary }}>{selectedCar.info.model}</Text>
       {/* Por enquanto apenas um título simples */}
     </View>
   );
@@ -22,10 +24,5 @@ export default function CarDetails() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-
-  title: {
-    flex: 1,
-    color: "#fff",
   },
 });
