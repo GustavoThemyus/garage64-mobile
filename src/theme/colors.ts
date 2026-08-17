fd87f5529f99551d654425328c7c429c39abc4cc
Gustavo Themyus <gustavothemyus@gmail.com>
2026-08-17 09:07:31 -0300

    feat: add theme colors object for the application

---
+++ src/theme/colors.ts
@@ -0,0 +1,29 @@
+export const colors = {
+  dark: {
+    background: "#131313",
+    surface: "#1E1E1E",
+    surfaceTranslucent: "rgba(30,30,30,0.4)",
+    border: "#333",
+    separator: "#747474",
+    textPrimary: "#ffffff",
+    textSecondary: "#888",
+    badgeBackground: "#4E4E4E",
+    badgeText: "#1E1E1E",
+    accent: "#e4c413",
+    shadow: "#000",
+  },
+  // No light theme for now. Provisional.
+  light: {
+    background: "#131313",
+    surface: "#1E1E1E",
+    surfaceTranslucent: "rgba(30,30,30,0.4)",
+    border: "#333",
+    separator: "#747474",
+    textPrimary: "#ffffff",
+    textSecondary: "#888",
+    badgeBackground: "#4E4E4E",
+    badgeText: "#1E1E1E",
+    accent: "#e4c413",
+    shadow: "#000",
+  },
+};
