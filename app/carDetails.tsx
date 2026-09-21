9e01953ad4901cd85ab8ade0e2e14ac5bb0c0b9f
Gustavo Themyus <gustavothemyus@gmail.com>
2026-09-21 16:01:30 -0300

    feat: create dictionary for car attributes

--- app/carDetails.tsx
+++ app/carDetails.tsx
@@ -3,19 +3,31 @@ import { carsMoch } from "@/src/data/carsMoch";
 import { Text, View, StyleSheet } from "react-native"; // Imports dos componentes do React Native
 import { useLocalSearchParams } from "expo-router";
 import { useTheme } from "@/src/theme/useTheme";
+import { dictionary } from "@/src/data/dictionary";
 
 // Função da tela de detalhes do carro.
 export default function CarDetails() {
   const theme = useTheme();
   const { carId } = useLocalSearchParams();
   const selectedCar = carsMoch.find((car) => car.info.id === Number(carId));
+
   if (!selectedCar) {
     return <Text>Not found</Text>;
   }
+
+  const carDetail = Object.entries(selectedCar.specs).map(
+    ([fieldName, fieldValue]) => (
+      <View key={fieldName}>
+        <Text style={{ color: theme.textPrimary }}>{fieldName}</Text>
+      </View>
+    ),
+  );
+
   return (
     <View style={[styles.container, { backgroundColor: theme.background }]}>
       <Text style={{ color: theme.textPrimary }}>{selectedCar.info.model}</Text>
       {/* Por enquanto apenas um título simples */}
+      <View>{carDetail}</View>
     </View>
   );
 }
---
+++ src/data/dictionary.ts
@@ -0,0 +1,39 @@
+export const dictionary = {
+  info: {
+    brand: "Brand",
+    model: "Model",
+    year: "Year",
+    color: "Color",
+    countryCode: "Country",
+    generation: "Generation",
+  },
+
+  specs: {
+    type: "Type",
+    engine: "Engine",
+    engineLayout: "Engine Layout",
+    valves: "Valves",
+    valvetrain: "Valve Train",
+    displacement: "Displacement",
+    traction: "Traction",
+    aspiration: "Aspiration",
+    weight: "Weight",
+    transmission: "Transmission",
+    wheels: "Wheels",
+    steering: "Steering",
+    brakes: "Brakes",
+  },
+
+  performance: {
+    torque: "Torque",
+    topSpeed: "Top Speed",
+    acceleration: "Acceleration",
+    hp: "HP",
+    consumption: "Consumption",
+  },
+
+  history: {
+    curiosities: "Curiosities",
+    appearances: "Appearances",
+  },
+};
